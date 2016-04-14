6fb83593a63269848f8ece1083c48fe425ac3af9
Rex Cheng <hkchengad@connect.ust.hk>
2016-04-15 01:25:12 +0800

    can get name now with simplehtmldom, a html parser

--- Grabber/main.php
+++ Grabber/main.php
@@ -1,15 +1,28 @@
 <?php
+/**
+ * A COMP2021 Project in HKUST
+ * Author:
+ *      Chan Nok Hin
+ *      Cheng Ho Kei 12219689
+ *      Sze Ka Yau
+ * Licensed under MIT license
+ */
+
+include('./lib/simple_html_dom.php');
+
 ini_set('display_errors', true);
 ini_set('safe_mode', false);
 ini_set('allow_url_fopen', true);
 ini_set('allow_url_include', true);
 
 $curl = curl_init();
-
 curl_setopt($curl, CURLOPT_URL, "https://w5.ab.ust.hk/wcq/cgi-bin/1530/subject/COMP");
+//Do not verify the site certificate
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$html_string = curl_exec($curl);
+curl_close($curl);
 
-curl_exec($curl);
-
-curl_close($curl)
+$html = str_get_html($html_string);
+echo $html->getElementById("classes")->find(".course", 0)->find("h2", 0)->plaintext;
 ?>
